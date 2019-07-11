6415e53ce7bda6ed02b26d0d119ada3675f4e120
gothhouse <jamiedang1349@gmail.com>
2019-07-11 16:19:52 -0700

    first iteration

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Heyo!", message: "It'll be okay babey. You're doing great!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
 }
 
