92632811cdc66b41b049123dd7145f97f7201731
gothhouse <jamiedang1349@gmail.com>
2019-07-12 08:58:06 -0700

    iteration 2

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         let selectedEmotion = sender.titleLabel?.text
         let emojiMessage = ["frustrated" : "It's okay, struggle is how we grow! You'll do great, but maybe a break is warranted.", "depressed" : "Cry it out my buddy. You'll get through this!"]
         //figure out how to get the values to show up
-        let alertController = UIAlertController(title: emojinal, message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "emojinal", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
