6b5b1f38040ae163918708d6f82993ec9c3922c7
gothhouse <jamiedang1349@gmail.com>
2019-07-12 09:31:20 -0700

    3rd iteration

--- emojinal/ViewController.swift
+++ emojinal/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    //consider how to print rando messages if one key has more values than other
+    let customMessages = ["frustrated" : ["You'll do great, sweaty, dont worry!", "breathe mdude, you'll be fiiine.", "Uh oh, me too buddy.", "Yikes!", "Kronks!", "Jinkies!", "Aaaaaagh!!!!!", "uh oh"], "depressed" : ["Go ahead and cry it out i guess", "me too dawg.", "keep trying! or take a break lol, woteva.", "oopsies", "ugh"]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,12 +21,14 @@ class ViewController: UIViewController {
     
     let emojis = ["😤" : "frustrated", "😭" : "depressed"]
     
+    
     @IBAction func showMessage(sender: UIButton) {
+        let number = Int.random(in: 0..<customMessages.values.count)
         let selectedEmotion = sender.titleLabel?.text
-        let emojiMessage = ["frustrated" : "It's okay, struggle is how we grow! You'll do great, but maybe a break is warranted.", "depressed" : "Cry it out my buddy. You'll get through this!"]
-        //figure out how to get the values to show up
-        let alertController = UIAlertController(title: "emojinal", message: emojis[selectedEmotion!], preferredStyle: UIAlertController.Style.alert)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[number]
+        let alertController = UIAlertController(title: "emojinal", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "bye", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
 
